51013e94df2f407e1e39880bd197502ecdaff242
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-02-09 21:11:14 +0530

    Changed the structure to comply with the roadmap

--- First-Contact/writing_first_program.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-
-// Priting the first and most basic thing we do while learning a programming language. ie. Hello World!
-int main(){
-    cout<<"Hello World!";
-}
