48462842820cf571c485ece3637db0076465dccd
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-02-14 19:45:19 +0530

    updated 06_number_pyramid.cpp
    Yesterday, I solved this question. However, I forgot to write the main function. Therefore, update the code to complete the file.

--- phase-1.5-exploring-patterns/06_number_pyramid.cpp
+++ phase-1.5-exploring-patterns/06_number_pyramid.cpp
@@ -93,5 +93,81 @@ void pattern_4(int n){
 }
 
 int main(){
-    pattern_4(5);
+    int choice;
+
+    cout<<R"(----------------------------
+    PATTERN 1
+    ----------------------------
+    1 
+    2 2
+    3 3 3
+    4 4 4 4
+    5 5 5 5 5
+    ----------------------------
+    ----------------------------
+    PATTERN 2
+    ----------------------------
+    1 1 1 1 1
+    2 2 2 2
+    3 3 3
+    4 4
+    5
+    ----------------------------
+    ----------------------------
+    PATTERN 3
+    ----------------------------
+            1
+          2 2
+        3 3 3
+      4 4 4 4
+    5 5 5 5 5
+    ----------------------------
+    ----------------------------
+    PATTERN 4
+    ----------------------------
+    1 1 1 1 1
+      2 2 2 2
+        3 3 3
+          4 4
+            5
+    ----------------------------
+    ----------------------------)"<<endl<<endl;
+
+    cout<<"Enter your choice: ";
+    while(true){
+        if (cin >> choice){
+            if((choice>=1) && (choice<=4)){
+                if(!cin.fail()){
+                    break;
+                } else{
+                    cout<<"Invalid range. Enter 1, 2, 3, or 4: ";
+                }
+            } 
+        } else {
+                cout<<"Invalid input. Please enter a digit (1-4): ";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    }
+
+    int limit;
+
+    cout<<"Enter the max limit of the pattern: ";
+    cin>>limit;
+    while (true)
+    {
+        if (!cin.fail()){
+        break;
+    } else{
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max());
+        cout<<"Enter integer value only (with no space): ";
+    }
+    }
+
+    if (choice == 1) pattern_1(limit);
+    else if (choice == 2) pattern_2(limit);
+    else if (choice == 3) pattern_3(limit);
+    else pattern_4(limit);
+    
 }
