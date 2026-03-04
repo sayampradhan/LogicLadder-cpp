4401de594d15111f4755cc2d3d3dd102cff998d8
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-04 22:10:32 +0530

    Update 07_pascal_triangle.cpp

--- phase-2-mathematics/07_pascal_triangle.cpp
+++ phase-2-mathematics/07_pascal_triangle.cpp
@@ -1,10 +1,54 @@
 #include<iostream>
+#include<limits>
+#include <iomanip>
+#include<string>
 using namespace std;
 
-void pascal_triangle(int n){
+// Refer Program 14 (Phase 1.5 -- Exploring Patterns )
 
+/*
+-------------------
+PASCAL'S TRIANGLE
+-------------------
+        1
+      1   1
+    1   2   1
+  1   3   3   1
+1   4   6   4   1
+-------------------
+*/
+
+void pascal_triangle(int n) {
+    int width = 4; 
+
+    for (int i = 1; i <= n; i++) {
+        int coefficient = 1;
+
+        for (int k = 0; k < n - i; k++) {
+            cout << string(width / 2, ' '); 
+        }
+
+        for (int j = 1; j <= i; j++) {
+            cout << setw(width) << coefficient;
+            coefficient = coefficient * (i - j) / j;
+        }
+        
+        cout << endl;
+    }
 }
 
-int main(){
-
+main(){
+    int n;
+    cout<<"Enter the no. of rows: ";
+    cin>>n;
+    while(true){
+        if(!cin.fail()) break;
+        else{
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout<<"Invalid Input! Enter Again: ";
+            cin>>n;
+        }
+    }
+    pascal_triangle(n);
 }
