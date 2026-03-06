05a501e6a5288c97660783a5185b767adeb77423
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-06 18:38:50 +0530

    Update 09_strong_number.cpp

--- phase-2-mathematics/09_strong_number.cpp
+++ phase-2-mathematics/09_strong_number.cpp
@@ -1,2 +1,26 @@
 #include<iostream>
 using namespace std;
+
+// Strong Number Checker
+/*
+--------------------EXAMPLE-----------------------
+INPUT: 145
+OUTPUT: TRUE (if 1! + 4! + 5! = 145)
+*/
+
+bool strong_number(int n){
+    int original_num = n;
+    int sum_factorial = 0;
+
+    while (n>0){
+        int digit = n % 10;
+        int factorial = 1;
+        for (int i = 1; i < digit + 1; i++){
+            factorial *= i;
+        }
+        sum_factorial += factorial;
+        n /= 10;
+    }
+    int res = sum_factorial == original_num;
+    return res;
+}
