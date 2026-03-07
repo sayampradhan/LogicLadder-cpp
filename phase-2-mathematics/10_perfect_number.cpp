0a8ff86bb92f2e6f1f3f6c7747715edb261ffea3
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-07 19:42:19 +0530

    Update 10_perfect_number.cpp

--- phase-2-mathematics/10_perfect_number.cpp
+++ phase-2-mathematics/10_perfect_number.cpp
@@ -15,21 +15,23 @@ using namespace std;
         ii. Otherwise, number is NOT perfect.
 */
 void perfect_number(int n){
+
+}
+
+main(){
+    int n = 6;
     vector<int> divisors = {};
     int sum = 0;
     int temp = 0;
     for (int i = 0; i <= n; i++){
-    if (n % i == 0){
-        int temp = i;
-        divisors.push_back(temp);
-    }
+        if (n % i == 0){
+            int temp = i;
+            divisors.push_back(temp);
+        }
     }
     for (int element: divisors){
-        cout<<"Element: "<<element
+        cout<<"Element: "<<element;
     }
 }
-
-main(){
-    perfect_number(5);
 
 }
