6321d67e06d1cb9cb19436500525292296824562
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-08 13:37:23 +0530

    Update 11_guessing_game.cpp

--- phase-2-mathematics/11_guessing_game.cpp
+++ phase-2-mathematics/11_guessing_game.cpp
@@ -20,7 +20,8 @@ BUILDING A GUESSING GAME
 int random_number(int start, int end){
     random_device rd;
     mt19937 gen(rd());
-
+    uniform_int_distribution<> distr(start, end);
+    return distr(gen);
 }
 
 main(){
