502a29fa1287625ae33c5c8d6f06980263c2a5e1
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-08 19:41:27 +0530

    Update 01_second_largest_element.cpp

--- phase-3-data-structures/01_second_largest_element.cpp
+++ phase-3-data-structures/01_second_largest_element.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 // Finding the second largest number in an array
 /*
-THREE ALGORITHMS used to solve this problem are:
+FOUR ALGORITHMS used to solve this problem are:
     1. Single-Pass Approach
     2. Two-Pass Approach
     3. Sorting Approach
