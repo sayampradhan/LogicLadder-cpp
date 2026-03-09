ccfdca9776d3bc8488acf73aae025d37323e1576
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-09 22:08:05 +0530

    Update 02_remove_duplicates.cpp

--- phase-3-data-structures/02_remove_duplicates.cpp
+++ phase-3-data-structures/02_remove_duplicates.cpp
@@ -1,2 +1,41 @@
 #include<iostream>
 using namespace std;
+
+bool isSorted(int arr[]){
+    int length = sizeof(arr)/sizeof(arr[0]);
+    for (int i = 0; i < length; i++){
+        if (arr[i]<arr[i+1]){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+
+void sortArray(int *arr){
+    int length = sizeof(arr)/sizeof(arr[0]);
+    int temp = 0;
+    for (int i = 0; i < length; i++){
+        if (arr[i]>arr[i+1]){
+            temp = arr[i];
+            int arr[i] = arr[i+1];
+        }
+        else{
+            arr[i+1] = arr[i];
+        }
+    }
+}
+
+// Function to remove duplicates from an array
+void removeDuplicates(int arr[], int size){
+    if (size == 0) return;
+    if (isSorted(arr)==true){
+
+    }
+}
+
+// main function
+main(){
+    int arr[] = {1,2,5,1,7};
+}
