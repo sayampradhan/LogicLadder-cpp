897cb39896eb6ebf94079ea3353c0f0dfac7e1cb
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-10 10:06:43 +0530

    Update 03_rotate_array.cpp

--- phase-3-data-structures/03_rotate_array.cpp
+++ phase-3-data-structures/03_rotate_array.cpp
@@ -1,2 +1,13 @@
 #include<iostream>
 using namespace std;
+
+/*
+We need to rotate an array left/right by k position.
+
+- The position k will be inputted from the user.
+- And the array will also inputed from the user.
+- Then perform the rotation
+
+Example:
+
+*/
