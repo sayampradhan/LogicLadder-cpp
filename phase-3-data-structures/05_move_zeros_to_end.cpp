47b9a41db8d8794d3ee78d4a940e6a2a2b99c8d6
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-12 23:47:29 +0530

    Update 05_move_zeros_to_end.cpp

--- phase-3-data-structures/05_move_zeros_to_end.cpp
+++ phase-3-data-structures/05_move_zeros_to_end.cpp
@@ -10,6 +10,15 @@ void moveZeros(vector<int> *arr, int size, int count){
 
 main(){
     vector<int> arr;
+    int size;
+    cout<<"Enter the length of the array: ";
+    cin>>size;
+    for (int i = 0; i < size; i ++){
+        
+    }
+
+
+
     int target = 0;
     int count = count(arr.begin(), arr.end(), target);
 }
