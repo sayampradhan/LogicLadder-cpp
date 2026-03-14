ed6aaebda3422f68c8604379f234d1391432344c
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-14 20:40:04 +0530

    Update 08_binary_search.cpp

--- phase-3-data-structures/08_binary_search.cpp
+++ phase-3-data-structures/08_binary_search.cpp
@@ -6,39 +6,33 @@ using namespace std;
 int binary_search(vector<int>& arr, int element){
     int low = 0;
     int high = arr.size() - 1;
-    int mid = (high - low) / 2;
-    bool found = false;
 
-    while (found != true){
-        for (int i = low; i < high; i++){
-            if(arr[mid]<element){
-                low = mid;
-                high = high;
-                mid = (high - low)/2;
-                
-                }
+    while(low <= high){
+        int mid = low + (high - low) / 2;
 
+        if(arr[mid] == element){
+            return mid;
         }
-
+        else if(arr[mid] < element){
+            low = mid + 1;
+        }
+        else{
+            high = mid - 1;
+        }
     }
 
-    
-
-
-
+    return -1;
 }
 
 int main(){
     vector<int> arr;
     int size;
-    int low;
-    int high;
-    int mid;
     int element;
 
     cout<<"Enter length of the array: ";
     cin>>size;
     cout<<endl<<endl;
+
     for(int i = 0; i < size; i++){
         int temp = 0;
         cout<<"Enter element at index "<<i<<" : ";
@@ -46,12 +40,20 @@ int main(){
         arr.push_back(temp);
     }
 
-    sort(arr.begin(), arr.end()); 
-    auto last = std::unique(arr.begin(), arr.end());
+    sort(arr.begin(), arr.end());
+    auto last = unique(arr.begin(), arr.end());
     arr.erase(last, arr.end());
-    
+
     cout<<endl<<endl;
     cout<<"Enter element to search: ";
     cin>>element;
-    binary_search(arr, element);
+
+    int result = binary_search(arr, element);
+
+    if(result != -1){
+        cout<<"Element found at index "<<result;
+    }
+    else{
+        cout<<"Element not found";
+    }
 }
